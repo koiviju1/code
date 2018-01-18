335040fcd6d02e74e4a8fdb19cd7f3856bcbe91d
Juha Koivistoinen <juha.koivistoinen@norian.eu>
2018-01-18 10:17:08 +0200

    redirect issue fix att

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,19 +2,19 @@
 angular.module('SpaTestMain',['ngRoute','AdalAngular']).config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
 
 		$routeProvider.when("/", {
-			controller: "homeCtrl",
+			controller: "/Scripts/mainCtrl.js",
 			templateUrl: "/index.html",
-		}).when("/", {
-			controller: "mainCtrl",
-			templateUrl: "/main.html",
+		}).when("/Main", {
+			controller: "/Scripts/mainCtrl.js",
+			templateUrl: "/Main/main.html",
 			requireADLogin: true,
-		}).otherwise({ redirectTo: "/main.html" });
+		}).otherwise({ redirectTo: "/Main/main.html" });
 		
 	    adalProvider.init(
         {
 			tenant: 'opuscapita.onmicrosoft.com',
             clientId: '43436228-9c32-4023-9753-48e0170e642f',
-			redirectUri: 'https://archive-main.azurewebsites.net/main.html',
+			redirectUri: 'https://archive-main.azurewebsites.net/Main/main.html',
 			instance: 'https://login.microsoftonline.com/', 
             popUp: false,
             cacheLocation: 'localStorage'
